3361770d1c10520f5dcb1d17add6265c51117cef
Rosie Hamilton <enchantrix@gmail.com>
2016-06-16 21:08:02 +0100

    started some new plots

--- survey_analysis_2.R
+++ survey_analysis_2.R
@@ -3,8 +3,8 @@
 # testingfuntime.blogspot.co.uk
 
 # Set working dir
-setwd ("/Dev/Git/tester_survey")
-#setwd("/git/tester_survey")
+#setwd ("/Dev/Git/tester_survey")
+setwd("~/git/tester_survey")
 
 # Read in data
 mydata <- read.csv("survey_results_raw.csv",
@@ -82,7 +82,7 @@ ind_data <- cbind(ind_data, combined)
 # Display all the RColourBrewer palettes
 library(RColorBrewer)
 display.brewer.all()
-?treemap
+
 # Treemap
 png(filename = "treeplot.png", width = 600, height = 500, units = "px")
 treemap(ind_data,
@@ -159,7 +159,6 @@ barplot(ind_comp,
 #are testers skills more likely to be valued the longer they have worked in testing
 
 
-
 lessthanone <- which(mydata[,14] == "less than a year")
 onetotwo <- which(mydata[,14] == "1 - 2 years")
 twotofive <- which(mydata[,14] == "2 - 5 years")
@@ -187,23 +186,24 @@ tentotwenty_skills <- droplevels(tentotwenty_skills)
 twentyplus_skills <- droplevels(twentyplus_skills)
 
 
+
 LTO <- table(lessthanone_skills)
-LTO_perc <- c(LTO[2] /(LTO[3]+ LTO[2]),LTO[3] /(LTO[3]+ LTO[2]))
+LTO_perc <- round(c(LTO[1] /(LTO[2]+ LTO[1]),LTO[2] /(LTO[2]+ LTO[1])), digits=2)
 
 OTT <- table(onetotwo_skills)
-OTT_perc <- c(OTT[2] /(OTT[3]+ OTT[2]),OTT[3] /(OTT[3]+ OTT[2]))
+OTT_perc <- round(c(OTT[1] /(OTT[2]+ OTT[1]),OTT[2] /(OTT[2]+ OTT[1])), digits=2)
 
 TTF <- table(twotofive_skills)
-TTF_perc <- c(TTF[2] /(TTF[3]+ TTF[2]),TTF[3] /(TTF[3]+ TTF[2]))
+TTF_perc <- round(c(TTF[1] /(TTF[2]+ TTF[1]),TTF[2] /(TTF[2]+ TTF[1])), digits=2)
 
 FTT <- table(fivetoten_skills)
-FTT_perc <- c(FTT[2] /(FTT[3]+ FTT[2]),FTT[3] /(FTT[3]+ FTT[2]))
+FTT_perc <- round(c(FTT[1] /(FTT[2]+ FTT[1]),FTT[2] /(FTT[2]+ FTT[1])), digits=2)
 
 TTT <- table(tentotwenty_skills)
-TTT_perc <- c(TTT[2] /(TTT[3]+ TTT[2]),TTT[3] /(TTT[3]+ TTT[2]))
+TTT_perc <- round(c(TTT[1] /(TTT[2]+ TTT[1]),TTT[2] /(TTT[2]+ TTT[1])), digits=2)
 
 TP <- table(twentyplus_skills)
-TP_perc <- c(TP[2] /(TP[3]+ TP[2]),TP[3] /(TP[3]+ TP[2]))
+TP_perc <- round(c(TP[1] /(TP[2]+ TP[1]),TP[2] /(TP[2]+ TP[1])), digits=2)
 
 LTO_perc
 OTT_perc
@@ -214,11 +214,84 @@ TP_perc
 
 #up to two years is less than 1 plus one to two
 UTT <- LTO + OTT
-UTT_perc <- c(UTT[2] /(UTT[3]+ UTT[2]),UTT[3] /(UTT[3]+ UTT[2]))
+UTT_perc <- c(UTT[1] /(UTT[2]+ UTT[1]),UTT[2] /(UTT[2]+ UTT[1]))
 
 
-
+#these bar plots look a bit rubbish
 barplot(c(UTT_perc, TTF_perc, FTT_perc, TTT_perc, TP_perc), col= rainbow(30))
+
+
+barplot(c(UTT, TTF, FTT, TTT, TP), col= rainbow(30))
+
+#ideally would like a percentage based stacked vertical barplot
+#grouped by duration testing, showing % of true and false reponses
+
+
+#tester qualifications
+
+None <-which(mydata[,17] == "None")
+GCSE <- which(mydata[,17] == "GCSEs or equivalent")
+AL <- which(mydata[,17] == "A-Levels or equivalent")
+FC<- which(mydata[,17] == "Foundation course")
+BD <- which(mydata[,17] == "Bachelors degree")
+MD <- which(mydata[,17] == "Masters degree")
+PHD <- which(mydata[,17] == "Doctorate")
+
+edu <- table(mydata[,17])
+
+
+N_sum <- length(None)
+G_sum <- length(GCSE)
+A_sum <- length(AL)
+F_sum <- length(FC)
+B_sum <- length(BD)
+M_sum <- length(MD)
+P_sum <- length(PHD)
+
+total <- N_sum+G_sum+A_sum+F_sum+B_sum+M_sum+P_sum
+
+plotcolnames <- c("None", "GCSE", "A-Level", "Foun.", "B.D", "M.D", "PHD") 
+
+
+#I would really like to add % labels to the top of each column
+
+barplot(c(N_sum, G_sum, A_sum, F_sum, B_sum, M_sum, P_sum), 
+        space = NULL, 
+        names.arg = plotcolnames,
+        col = rainbow(10), 
+        ylim = c(0,90),
+        xlab="Highest Qualification", 
+        ylab="Frequency",
+        main="Education levels"
+)
+axis(2,at=seq(0,90,10))
+
+phd_perc <- round((P_sum / total)*100, digits =4)
+bd_perc <-  round((B_sum / total)*100, digits =4)
+
+
+
+no_degree <- (N_sum + G_sum + A_sum + F_sum)
+degree <- (B_sum + M_sum + P_sum)
+
+# Make a vector containing total numbers with and without degree
+has_degree <- c(no_degree, degree) 
+# Generate labels for pie chart containing % of Happy and Not Happy
+pielabels <- c("Without Degree", "With Degree")
+percent <- round(has_degree/sum(has_degree)* 100, digits = 1)
+pielabels <- paste(pielabels, percent)    
+pielabels <- paste(pielabels, "%", sep="")
+
+
+#Plot a pie chart 
+pie(has_degree, 
+    labels = pielabels, 
+    col = rainbow(7, start =0.35),
+    main = "Are testers graduates?")
+
+
+
+#tester training
 
 
 
